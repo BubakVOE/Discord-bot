4fbadb062498de480146e5b7f061e1e26c4f24af
ludwig-dev <bubakvoe@gmail.com>
2024-09-20 22:42:27 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import Log from '@/helper/Logging';
-// import InitService from './services/InitService';
 
 async function startServices() {
     Log('[Server] starting');
-
-    // await InitService();
 }
 
 startServices();
---
+++ tsconfig-paths-bootstrap.js
@@ -0,0 +1,16 @@
+const tsConfigPaths = require('tsconfig-paths');
+const tsConfig = require('./tsconfig.json');
+const path = require('path');
+
+const baseUrl = path.resolve(__dirname, tsConfig.compilerOptions.outDir);
+const paths = {};
+
+for (const alias in tsConfig.compilerOptions.paths) {
+  paths[alias] = tsConfig.compilerOptions.paths[alias].map(p => p.replace('src', 'dist'));
+}
+
+console.log('Registering paths:', { baseUrl, paths });
+
+tsConfigPaths.register({ baseUrl, paths });
+
+require('./dist/server.js');
