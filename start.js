936a9ae00f0864556c62394970542593fdd1130e
ludwig-dev <bubakvoe@gmail.com>
2024-09-20 22:51:22 +0200

    Fix absolute path

--- start.js
+++ start.js
@@ -1,15 +1,12 @@
 const tsConfigPaths = require('tsconfig-paths');
-const tsConfig = require('./tsconfig.json');
-const path = require('path');
 
-console.log('Registering paths:', {
-  baseUrl: path.resolve(__dirname, tsConfig.compilerOptions.outDir),
-  paths: tsConfig.compilerOptions.paths
-});
+require('module-alias/register');
 
 tsConfigPaths.register({
-  baseUrl: path.resolve(__dirname, tsConfig.compilerOptions.outDir),
-  paths: tsConfig.compilerOptions.paths
+    baseUrl: './dist',
+    paths: {
+        '@/*': ['./*']
+    }
 });
 
 require('./dist/server.js');
